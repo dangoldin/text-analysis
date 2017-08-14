0985331af0b227315ba3be67badcb8dbf1ff8d75
Dan Goldin <dgoldin@triplelift.com>
2017-08-13 21:28:59 -0400

    Add sentence analysis. Start messing with nltk

--- analyze.py
+++ analyze.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import sys
 import re
 from collections import Counter
+from nltk.tokenize import sent_tokenize
 
 def read_file(file):
     with open(file, 'r') as f:
         return f.read()
 
+def get_sentences(text):
+    # return sent_tokenize(text.decode('utf-8'))
+    return sent_tokenize(text)
+
 def get_words(text):
     return [w.lower() for w in re.findall(r'\w+', text)]
 
-def get_stats(words):
+def get_num_words(text):
+    return len(get_words(text))
+
+def get_word_stats(words):
     s = sum([len(w) for w in words])
-    print 1.0 * s/len(words)
+    print 'Average word length', 1.0 * s/len(words)
 
     c = Counter(words)
-    print c.most_common(20)
+    print 'Most common words', c.most_common(20)
 
     longest = sorted(list(set(words)), key=lambda w: len(w), reverse=True)
-    print longest[:20]
+    print 'Longest words', longest[:20]
+
+def get_sentence_stats(sentences):
+    print 'Num sentences', len(sentences)
+    print 'Average sentence length - chars', 1.0*sum(len(s) for s in sentences)/len(sentences)
+    print 'Average sentence length - words', 1.0*sum(get_num_words(s) for s in sentences)/len(sentences)
 
 if __name__ == '__main__':
     files = sys.argv[1:]
@@ -28,4 +42,6 @@ if __name__ == '__main__':
         print 'Processing', file
         text = read_file(file)
         words = get_words(text)
-        get_stats(words)
+        sentences = get_sentences(text)
+        get_word_stats(words)
+        get_sentence_stats(sentences)
