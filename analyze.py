9f89eeeff98883a33a7803785feceae7b58a52ef
Dan Goldin <dgoldin@triplelift.com>
2017-08-18 22:41:02 -0400

    Minor text cleanup

--- analyze.py
+++ analyze.py
@@ -9,6 +9,9 @@ from nltk.corpus import stopwords
 
 stop = set(stopwords.words('english'))
 
+def print_spacing(text, spacing = ''):
+    print spacing, text
+
 def read_file(file):
     with open(file, 'r') as f:
         return f.read()
@@ -24,24 +27,23 @@ def get_num_words(text):
 
 def get_word_stats(words):
     s = sum([len(w) for w in words])
-    print 'Average word length', 1.0 * s/len(words)
+    print 'Average word length:', 1.0 * s/len(words)
 
     c = Counter(words)
-    print 'Most common words', c.most_common(20)
+    print 'Most common words:', c.most_common(20)
 
     c2 = Counter(zip(words, words[1:]))
-    print 'Most common word pairs', c2.most_common(20)
+    print 'Most common word pairs:', c2.most_common(20)
 
     longest = sorted(list(set(words)), key=lambda w: len(w), reverse=True)
-    print 'Longest words', longest[:20]
-
-    print 'Unique words', len(set(words))
-    print 'Unique word density', 1.0*len(set(words))/len(words)
+    print 'Longest words:', longest[:20]
+    print 'Unique words:', len(set(words))
+    print 'Unique word density:', 1.0*len(set(words))/len(words)
 
 def get_sentence_stats(sentences):
-    print 'Num sentences', len(sentences)
-    print 'Average sentence length - chars', 1.0*sum(len(s) for s in sentences)/len(sentences)
-    print 'Average sentence length - words', 1.0*sum(get_num_words(s) for s in sentences)/len(sentences)
+    print 'Num sentences:', len(sentences)
+    print 'Average sentence length - chars:', 1.0*sum(len(s) for s in sentences)/len(sentences)
+    print 'Average sentence length - words:', 1.0*sum(get_num_words(s) for s in sentences)/len(sentences)
 
 if __name__ == '__main__':
     FILES = sys.argv[1:]
